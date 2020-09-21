9c588b0b747e44fc07938cdc03ffa652cdee0afa
Carol Bezerra <carol.arbe@gmail.com>
2020-09-21 20:14:18 -0300

    pixel art project

--- 1.WebDevelopment/5.DOM-Selectors/Pixels-Art/script.js
+++ 1.WebDevelopment/5.DOM-Selectors/Pixels-Art/script.js
@@ -38,9 +38,8 @@ for (let i = 0; i < 4; i += 1) {
 function deleteAll() {
 	// apagar tudo para fazer conforme o usuário desejar
 	while (document.querySelector('.pixel-board').firstChild) {
-		document
-			.querySelector('.pixel-board')
-			.removeChild(document.querySelector('.pixel-board').firstChild);
+		document.querySelector('.pixel-board')
+		document.removeChild(document.querySelector('.pixel-board').firstChild);
 	}
 }
 
