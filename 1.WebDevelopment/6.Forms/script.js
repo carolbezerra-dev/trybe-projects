ecdfd4c87f5eabb5e8a1922899923a03c2a30987
Carol Bezerra <carol.arbe@gmail.com>
2020-10-16 10:09:36 -0300

    Facebook Project

--- 1.WebDevelopment/6.Forms/script.js
+++ 1.WebDevelopment/6.Forms/script.js
@@ -8,8 +8,10 @@ const btnRegister = document.querySelector('#facebook-register');
 
 function emptyInput(event) { // função para saber se tem algum input vazio
   event.preventDefault(); // para que não atualize a página ao clique do botão
-  const errorMessage = document.createElement('p');
-  document.querySelector('.right-content').appendChild(errorMessage);
+  // const errorMessage = document.createElement('p');
+  // document.querySelector('.right-content').appendChild(errorMessage);
+  let finalMessage = document.querySelector('.quick-easy');
+  finalMessage.classList.add('finalMessage');
   const inputGender = document.querySelectorAll('input[name="gender"]');
   let gender;
   for (let i = 0; i < inputGender.length; i += 1) {
@@ -19,21 +21,23 @@ function emptyInput(event) { // função para saber se tem algum input vazio
   }
   for (let i = 0; i < allInputs.length; i += 1) {
     if (allInputs[i].value === '') {
-      errorMessage.innerText = 'Invalid fields';
+      finalMessage.innerText = 'Invalid fields';
     } else {
       const phoneMail = document.querySelector('#phone_email').value;
       const birthDay = document.querySelector('#birthday').value;
       const firstName = document.querySelector('#first-name').value;
       const lastName = document.querySelector('#last-name').value;
       const printAll = `Hello, ${firstName} ${lastName}!
-      Gender: ${gender}
-      Phone or e-mail: ${phoneMail}
+      Gender: ${gender};
+      Phone or e-mail: ${phoneMail};
       Birthday: ${birthDay}`;
-      errorMessage.innerHTML = `${printAll}`;
+      finalMessage.innerHTML = `${printAll}`;
     }
   }
   const formData = document.querySelector('.cadastro'); // para deletar o form
   formData.parentNode.removeChild(formData);
+  document.querySelector('.right-content').removeChild(document.querySelector('.create-page'));
+  document.querySelector('.right-content').removeChild(document.querySelector('.right-content-h1'));
 }
 btnRegister.addEventListener('click', emptyInput); // ao clicar faz a verificação
 
